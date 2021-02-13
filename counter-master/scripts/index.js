311636c4eba86250c6801719ae93d35313e75d77
Srinivasan M <srinivijay143@gmail.com>
2021-02-13 23:19:13 +0530

    Update index.js

--- counter-master/scripts/index.js
+++ counter-master/scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 14, 2021',
+  birthdate: 'Feb 15, 2021',
   name: 'Sonia'
 };
 
