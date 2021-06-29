15c719f051d90195263ae2fe77b15fed6c3c1776
Srinivijay77 <50356279+Srinivijay77@users.noreply.github.com>
2021-06-29 12:24:13 +0530

    sbday

--- s/scripts/index.js
+++ s/scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jun 29, 2021',
-  name: 'Rajalakshmi'
+  birthdate: 'Feb 14, 2021',
+  name: 'Sonia'
 };
 
 function hideEverything() {
