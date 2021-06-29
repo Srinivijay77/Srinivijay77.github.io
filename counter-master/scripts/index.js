4f8767ecdc0cc0773c714fc86fb4c9337aeb24bf
Srinivijay77 <50356279+Srinivijay77@users.noreply.github.com>
2021-06-29 12:14:21 +0530

    birthday

--- counter-master/scripts/index.js
+++ counter-master/scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 1, 2021',
-  name: 'Sonia'
+  birthdate: 'Jun 29, 2021',
+  name: 'Rajalakshmi'
 };
 
 function hideEverything() {
